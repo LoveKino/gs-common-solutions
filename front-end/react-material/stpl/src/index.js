17dc44562b2365f8d53ea2c7820a952e886f7c14
loveKino <842914439@qq.com>
2018-05-05 16:12:58 +0800

    #auto-sync

--- front-end/react-material/stpl/src/index.js
+++ front-end/react-material/stpl/src/index.js
@@ -2,67 +2,105 @@
 
 import React from 'react'; // eslint-disable-line
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'; // eslint-disable-line
-import AppBar from 'material-ui/AppBar'; // eslint-disable-line
+import { BrowserRouter as BrowserRouter, Router, Route, Link } from 'react-router-dom'; // eslint-disable-line
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'; // eslint-disable-line
-import FlatButton from 'material-ui/FlatButton'; // eslint-disable-line
+
+import FilterPage from './page/filterPage'; // eslint-disable-line
+import SettingPage from './page/settingPage'; // eslint-disable-line
+
+import PageLoader from './view/pageLoader'; // eslint-disable-line
+import SideBar from './view/sidebar'; // eslint-disable-line
+import AppBar from './view/appbar'; // eslint-disable-line
+
+import {storeWithLoading} from './util/loadingStore';
+import ErrorRetryView from './view/errorRetryView'; // eslint-disable-line
+
+import Store from './store';
+
 import '../style/index.css';
 
 // material UI: http://www.material-ui.com
 // react router: https://reacttraining.com/react-router
 
-class Page1 extends React.Component {
-    constructor() {
-        super();
-        this.state = {};
-    }
-
-    render() {
-        return (<MuiThemeProvider>
-            <div>
-                This is page 1
-                <Link to="/page2">
-                    <FlatButton label="to page2" />{' '}
-                </Link>
-            </div>
-        </MuiThemeProvider>);
-    }
-}
-
-class Page2 extends React.Component {
-    constructor() {
-        super();
-        this.state = {};
-    }
-
-    render() {
-        return ( <div >
-            This is page 2
-        </div>);
-    }
-}
-
 /**
  * config router
  */
-const Main = () => ( // eslint-disable-line
-    <Router>
-        <div>
-            <MuiThemeProvider>
-                <div>
-                    <AppBar
-                        title="react material demo"
-                        onLeftIconButtonClick={() => location.href = '/'}
-                        onTitleClick={() => location.href = '/'}
-                        iconElementRight={<div />}
-                    />
-                </div>
-            </MuiThemeProvider>
+class Main extends React.Component { // eslint-disable-line
+    constructor() {
+        super();
+        this.state = {
+            group: 0,
+            groups: ['User Group', 'Product Group'],
 
-            <Route exact path="/" component={Page1} />
-            <Route path="/page2" component={Page2} />
-        </div>
-    </Router>
-);
+            country: 0,
+            countryList: [],
+
+            channelCategory: [],
+
+            pageError: null
+        };
+    }
+
+    componentDidMount() {
+        const self = this;
+        storeWithLoading(Store.combo([Store.getUserInfo, Store.getChannelCategory]), self)().then(([user, channelCategory]) => {
+            self.setState({
+                countryList: user.access,
+                channelCategory
+            });
+        });
+    }
+
+    updateUserData() {
+        const self = this;
+        return storeWithLoading(Store.getUserInfo, self)().then((user) => {
+            self.setState({
+                countryList: user.access
+            });
+        });
+    }
+
+    handleCountryChange(index) {
+        this.setState({
+            country: index
+        });
+    }
+
+    handleGroupChange(index) {
+        this.setState({
+            group: index
+        });
+    }
+
+    render() {
+        return (<BrowserRouter>
+            <div>
+                <MuiThemeProvider>
+                    <div>
+                        <AppBar/>
+                        <SideBar
+                            country={this.state.country}
+                            countryList={this.state.countryList}
+                            onChangeCountry={(index)=>this.handleCountryChange(index)}
+
+                            group={this.state.group}
+                            groups={this.state.groups}
+                            onChangeGroup={(index)=>this.handleGroupChange(index)}/>
+                        {this.state.showPageLoader?<PageLoader/> :null}
+                        <ErrorRetryView onRetry={this.updateUserData.bind(this)} error={this.state.pageError} onClose={()=>this.setState({pageError: null})}/>
+                    </div>
+                </MuiThemeProvider>
+
+                <Route exact path="/" component={()=><div style={{marginLeft:256, marginTop:64}}>
+                    <FilterPage
+                        channelCategory={this.state.channelCategory}
+                        country={this.state.country}
+                        countryList={this.state.countryList}/>
+                </div>} />
+                <Route exact path="/setting" component={()=><div style={{marginLeft:256, marginTop:64}}><SettingPage/></div>} />
+            </div>
+        </BrowserRouter>);
+    }
+}
 
 ReactDOM.render(<Main />, document.getElementById('pager'));
--- tool/webpack/stpl/webpack.config.js
+++ tool/webpack/stpl/webpack.config.js
@@ -3,43 +3,57 @@
 const webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        app: ['./src/index.js']
-    },
-    devtool: 'source-map',
-    output: {
-        path: __dirname + '/assets',
-        filename: 'app.js',
-        publicPath: 'js'
-    },
+  mode: process.env.DEV ? 'development' : 'production',
+  entry: {
+    app: ['./src/index.js']
+  },
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/dist/assets',
+    filename: 'app.js',
+    publicPath: '/assets'
+  },
 
-    module: {
-        rules: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['react', 'babel-preset-env'],
-                    plugins: ['wildcard']
-                }
-            }
-        }, {
-            test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'url?limit=10000&mimetype=application/font-woff'
-        }, {
-            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'url?limit=10000&mimetype=application/octet-stream'
-        }, {
-            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'file'
-        }, {
-            test: /\.svg/,
-            loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
-        }]
-    },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'babel-preset-env'],
+          plugins: ['wildcard']
+        }
+      }
+    }, {
+      test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&mimetype=application/font-woff'
+    }, {
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&mimetype=application/octet-stream'
+    }, {
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'file'
+    }, {
+      test: /\.svg/,
+      loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+    }]
+  },
 
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ]
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+
+  devServer: {
+    contentBase: __dirname + '/dist',
+    hot: true,
+    progress: true,
+    port: 8080,
+    proxy: {},
+    watchContentBase: true,
+    watchOptions: {
+      poll: true,
+      ignored: /node_modules/
+    }
+  }
 };
